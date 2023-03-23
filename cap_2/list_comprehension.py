7153917b360f5fe70456656ca97c61d2d7db61e9
francosoa <francosoamota@gmail.com>
2023-03-23 20:11:43 -0300

    excluindo arquivos

--- cap_2/list_comprehension.py
+++
@@ -1,8 +0,0 @@
-#Produto cartesiano
-colors = ['black', 'white']
-sizes = ['S', 'M', 'L']
-tshirts = [(color, size) for color in colors for size in sizes]
-
-
-#1) Gera uma lista de tuplas organizadas por cor e, em seguida, por tamanho.
-tshirts_sorted = [sorted((color, size)) for color in colors for size in sizes]
